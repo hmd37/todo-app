fd678fdf85c1fd031401c4ac11d97e6de5a74d81
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2024-12-31 23:52:06 +0400

    added login/logout in navbar

--- todo/accounts/views.py
+++ todo/accounts/views.py
@@ -29,5 +29,5 @@ def login_view(request):
 def logout_view(request):
     if request.method == "POST":
         logout(request)
-        return redirect("todolist-list")
+        return redirect("accounts:login")
     
