4b29ceccfb37849f91ff887f867344085bb46245
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2025-01-11 19:32:03 +0400

    commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+
+def redirect_to_lists(request):
+    return redirect('/todo/lists/')
 
 urlpatterns = [
+    path('', redirect_to_lists),
     path('admin/', admin.site.urls),
     path('todo/', include('tasks.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
