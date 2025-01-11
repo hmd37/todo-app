6dbfe68463e37010e4f1e86bcec27459efead714
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2025-01-11 17:55:28 +0400

    commit

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ set -o errexit
 pipenv install --deploy --ignore-pipfile
 
 # Activate Pipenv environment
-pipenv run python manage.py collectstatic --no-input
-pipenv run python manage.py migrate
+pipenv run python todo/manage.py collectstatic --no-input
+pipenv run python todo/manage.py migrate
 
 # Create superuser if environment variables are set
 if [[ $CREATE_SUPERUSER ]]; then
-  pipenv run python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+  pipenv run python todo/manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
